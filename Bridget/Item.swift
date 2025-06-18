ad46b83ba292cda0a5c05dcfacae5741ae63c43f
Peter Jemley <PeterJemley@users.noreply.github.com>
2025-06-18 16:24:00 -0700

    Initial Commit

---
+++ Bridget/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bridget
+//
+//  Created by Peter Jemley on 6/18/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BridgetTests/BridgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BridgetTests.swift
+//  BridgetTests
+//
+//  Created by Peter Jemley on 6/18/25.
+//
+
+import Testing
+@testable import Bridget
+
+struct BridgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
